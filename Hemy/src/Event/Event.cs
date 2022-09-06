4378f017d53d4b6b4a5b6f3dfb269d47b66e8549
Davi Portela <nyancraftmain@gmail.com>
2022-09-06 16:25:59 -0300

    Setup, Again?

---
+++ Hemy/src/Event/Event.cs
@@ -0,0 +1,20 @@
+namespace Hemy.Event;
+
+public enum EventType
+{
+    None = 0,
+    WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+    AppTick, AppUpdate, AppRender,
+    KeyPressed, KeyReleased,
+    MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+}
+
+public enum EventCategory
+{
+    None = 0,
+    EventCategoryApplication = 1 << 0,
+    EventCategoryInput = 1 << 1,
+    EventCategoryKeyboard = 1 << 2,
+    EventCategoryMouse = 1 << 3,
+    EventCategoryMouseButton = 1 << 4
+}
